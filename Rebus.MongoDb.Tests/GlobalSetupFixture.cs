cfc0203b780fca297d2b5e37bae330b5abc5a33f
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2024-09-16 16:53:35 +0200

    updated mongodb drivers (To signed version)

---
+++ Rebus.MongoDb.Tests/GlobalSetupFixture.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using NUnit.Framework;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Rebus.MongoDb.Tests
+{
+    [SetUpFixture]
+    public class GlobalSetupFixture
+    {
+        internal static List<String> DatabaseCreated = new List<String>();
+
+        [OneTimeSetUp]
+        public void This_is_run_before_ANY_tests() 
+        {
+            FixSerialzierForDriver2_19();
+        }
+
+         private static void FixSerialzierForDriver2_19()
+        {
+            try
+            {
+                if (!IsSerializerRegistered(typeof(object)))
+                {
+                    //After version 2.19 of the driver. https://github.com/mongodb/mongo-csharp-driver/releases/tag/v2.19.0
+                    var objectSerializer = new ObjectSerializer(type => ObjectSerializer.AllAllowedTypes(type));
+                    BsonSerializer.RegisterSerializer(objectSerializer);
+                }
+            }
+            catch (Exception)
+            {
+                //ignore errors because the serializer could be already registered.
+            }
+        }
+
+        internal static bool IsSerializerRegistered(Type type)
+        {
+            var serializerRegistry = BsonSerializer.SerializerRegistry;
+            var cacheInfo = typeof(BsonSerializerRegistry).GetField("_cache", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?? throw new InvalidOperationException("BsonSerializerRegistry._cache field not found. Internal implementation changed!");
+            var cache = cacheInfo.GetValue(serializerRegistry) as ConcurrentDictionary<Type, IBsonSerializer>;
+            return cache.ContainsKey(type);
+        }
+    }
+}
